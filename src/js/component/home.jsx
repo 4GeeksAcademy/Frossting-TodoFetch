9fe7b76000b482eb6729536e6dca976def5b4d54
Frossting <109557017+Frossting@users.noreply.github.com>
2023-09-13 13:56:15 +0000

    finished TodoList with fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,134 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let urlBase = "https://playground.4geeks.com/apis/fake/todos/user/davidbetancourt"
 
-//create your first component
-const Home = () => {
+function Home() {
+	const [task, setTask] = useState({ label: "", done: false });
+	const [tasksList, setTasksList] = useState([]);
+
+	const handleAddTask = async (event) => {
+		if (task && event.key == "Enter") {
+			//setTasksList([...tasksList, task]);
+			//setTask('');          
+			try {
+				let response = await fetch(urlBase, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify([...tasksList, task])
+				})
+				if (response.ok) {
+					getTask()
+				}
+			} catch (error) {
+
+			}
+		}
+	};
+
+	const taskChange = (event) => {
+		setTask({
+			...task,
+			label: event.target.value
+		})
+	}
+
+	const deleteTask = async (index) => {
+		const newList = tasksList.filter((value, i) => i !== index)
+		try {
+			let response = await fetch(urlBase, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(newList)
+			})
+			if (response.ok) {
+				getTask()
+			}
+		} catch (error) {
+
+		}
+	}
+
+	const getTask = async () => {
+		try {
+			let response = await fetch(urlBase)
+			let data = await response.json()
+			if (response.ok) {
+				setTasksList(data)
+			} else {
+				createUser()
+			}
+		} catch (error) {
+
+		}
+	}
+	const createUser = async () => {
+		try {
+			let response = await fetch(urlBase, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify([])
+			})
+			if (response.ok) {
+				getTask()
+			}
+
+		} catch (error) {
+
+		}
+	}
+
+
+	useEffect(() => {
+		getTask()
+	}, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<h1 className='title'>Todo List</h1>
+				<div className='col-12 p-0'>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Nueva tarea"
+						value={task.label}
+						onChange={e => taskChange(e)}
+						onKeyUp={e => handleAddTask(e)}
+					/>
+				</div>
+				<ul className="list-group col-12 p-0">
+					{tasksList.map((task, index) => (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between">
+							<span>{task.label}</span>
+							<button
+								onClick={e => deleteTask(index)}
+								style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}
+							>
+								<i class="far fa-trash-alt"></i>
+							</button>
+						</li>
+					))}
+				</ul>
+				{tasksList.length > 0 ? (
+					<div>
+						{tasksList.length} item{tasksList.length === 1 ? '' : 's'} left
+					</div>
+				) : (
+					<div>
+						No hay tareas, añadir tareas
+					</div>
+				)}
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
